9fbd545ee277aa8106be4c33dfe8c01b91336d51
C3real-kill3r <brybzi@gmail.com>
2019-09-23 16:00:18 +0300

    edit launch screen and add sms permission

--- Farmhub/Farmhub/app/src/main/java/com/brybz/farmhub/MainActivity.java
+++ Farmhub/Farmhub/app/src/main/java/com/brybz/farmhub/MainActivity.java
@@ -26,15 +26,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
-        editTextPhoneNumber=(EditText)findViewById(R.id.editTextPhoneNumber);
-        editTextName=(EditText)findViewById(R.id.editTextName);
-        editTextQuestion=(EditText)findViewById(R.id.editTextQuestion);
-        editTextLocation=(EditText)findViewById(R.id.editTextLocation);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        editTextName = findViewById(R.id.editTextName);
+        editTextQuestion = findViewById(R.id.editTextQuestion);
+        editTextLocation = findViewById(R.id.editTextLocation);
 
-        radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
+        radioGroup = findViewById(R.id.radioGroup);
 
-        Button button2 = (Button) findViewById(R.id.button2);
+        Button button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button=(Button)findViewById(R.id.button);
+        button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,33 +56,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     protected void sendSMSMessage() {
         Log.i("Send SMS", "");
-        String name=editTextName.getText().toString();
+        String name = editTextName.getText().toString();
         String phoneNo = editTextPhoneNumber.getText().toString();
-        String location= editTextLocation.getText(). toString();
+        String location = editTextLocation.getText().toString();
         String question = editTextQuestion.getText().toString();
-        String email=editTextEmail.getText().toString();
+        String email = editTextEmail.getText().toString();
         //get selected radioButton from RadioGroup
-        int selectedId=radioGroup.getCheckedRadioButtonId();
+        int selectedId = radioGroup.getCheckedRadioButtonId();
         //find the radioButton by returned id
-        radioButton=(RadioButton)findViewById(selectedId);
-        String farmingType=radioButton.getText().toString();
+        radioButton = findViewById(selectedId);
+        String farmingType = radioButton.getText().toString();
 
-        if (farmingType.equalsIgnoreCase("Maize Farming")){
-            expertPhoneNumber="0733717162";
-        }
-        else{
-            expertPhoneNumber="0723701894";
+        if (farmingType.equalsIgnoreCase("Maize Farming")) {
+            expertPhoneNumber = "0721336787";
+        } else {
+            expertPhoneNumber = "0710160339";
         }
         try {
             SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(expertPhoneNumber, null,name+","+phoneNo+" from "+location+" asks: "+question, null, null);
-            smsManager.sendTextMessage(phoneNo, null, "Your question has been sent to an expert in "+farmingType+" on Mobile No: "+expertPhoneNumber+". The expert will respond to your enquiry as soon as possible", null, null);
+            smsManager.sendTextMessage(expertPhoneNumber, null, name + "," + phoneNo + " from " + location + " asks: " + question, null, null);
+            smsManager.sendTextMessage(phoneNo, null, "Your question has been sent to an expert in " + farmingType + " on Mobile No: " + expertPhoneNumber + ". The expert will respond to your enquiry as soon as possible", null, null);
             Toast.makeText(getApplicationContext(), "Query Sent", Toast.LENGTH_LONG).show();
-        }
-
-        catch (Exception e) {
+        } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "SMS failed, please try again.", Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
